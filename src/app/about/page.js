9ef225761a1d3519bd09ef0ccb0a4c6b7b70fede
Nishant Singh <nishantbihar529@gmail.com>
2023-08-08 21:02:51 +0530

    feat: add navbar

---
+++ src/app/about/page.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4 p-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Sit ipsum exercitation magna deserunt Lorem quis dolor. Adipisicing
+        nostrud sint elit enim consectetur qui ad sunt. Eu minim enim qui
+        reprehenderit qui. Occaecat ex aliquip reprehenderit id eiusmod sint
+        consequat consectetur aliqua quis aliquip. Sit ipsum exercitation magna
+        deserunt Lorem quis dolor. Adipisicing nostrud sint elit enim
+        consectetur qui ad sunt. Eu minim enim qui reprehenderit qui. Occaecat
+        ex aliquip reprehenderit id eiusmod sint consequat consectetur aliqua
+        quis aliquip. Sit ipsum exercitation magna deserunt Lorem quis dolor.
+        Adipisicing nostrud sint elit enim consectetur qui ad sunt. Eu minim
+        enim qui reprehenderit qui. Occaecat ex aliquip reprehenderit id eiusmod
+        sint consequat consectetur aliqua quis aliquip. Sit ipsum exercitation
+        magna deserunt Lorem quis dolor. Adipisicing nostrud sint elit enim
+        consectetur qui ad sunt. Eu minim enim qui reprehenderit qui. Occaecat
+        ex aliquip reprehenderit id eiusmod sint consequat consectetur aliqua
+        quis aliquip. Sit ipsum exercitation magna deserunt Lorem quis dolor.
+        Adipisicing nostrud sint elit enim consectetur qui ad sunt. Eu minim
+        enim qui reprehenderit qui. Occaecat ex aliquip reprehenderit id eiusmod
+        sint consequat consectetur aliqua quis aliquip. Sit ipsum exercitation
+        magna deserunt Lorem quis dolor. Adipisicing nostrud sint elit enim
+        consectetur qui ad sunt. Eu minim enim qui reprehenderit qui. Occaecat
+        ex aliquip reprehenderit id eiusmod sint consequat consectetur aliqua
+        quis aliquip.
+      </p>
+      <p>
+        Sit ipsum exercitation magna deserunt Lorem quis dolor. Adipisicing
+        nostrud sint elit enim consectetur qui ad sunt. Eu minim enim qui
+        reprehenderit qui. Occaecat ex aliquip reprehenderit id eiusmod sint
+        consequat consectetur aliqua quis aliquip. Sit ipsum exercitation magna
+        deserunt Lorem quis dolor. Adipisicing nostrud sint elit enim
+        consectetur qui ad sunt. Eu minim enim qui reprehenderit qui. Occaecat
+        ex aliquip reprehenderit id eiusmod sint consequat consectetur aliqua
+        quis aliquip. Sit ipsum exercitation magna deserunt Lorem quis dolor.
+        Adipisicing nostrud sint elit enim consectetur qui ad sunt. Eu minim
+        enim qui reprehenderit qui. Occaecat ex aliquip reprehenderit id eiusmod
+        sint consequat consectetur aliqua quis aliquip. Sit ipsum exercitation
+        magna deserunt Lorem quis dolor. Adipisicing nostrud sint elit enim
+        consectetur qui ad sunt. Eu minim enim qui reprehenderit qui. Occaecat
+        ex aliquip reprehenderit id eiusmod sint consequat consectetur aliqua
+        quis aliquip. Sit ipsum exercitation magna deserunt Lorem quis dolor.
+        Adipisicing nostrud sint elit enim consectetur qui ad sunt. Eu minim
+        enim qui reprehenderit qui. Occaecat ex aliquip reprehenderit id eiusmod
+        sint consequat consectetur aliqua quis aliquip. Sit ipsum exercitation
+        magna deserunt Lorem quis dolor. Adipisicing nostrud sint elit enim
+        consectetur qui ad sunt. Eu minim enim qui reprehenderit qui. Occaecat
+        ex aliquip reprehenderit id eiusmod sint consequat consectetur aliqua
+        quis aliquip. Sit ipsum exercitation magna deserunt Lorem quis dolor.
+        Adipisicing nostrud sint elit enim consectetur qui ad sunt. Eu minim
+        enim qui reprehenderit qui. Occaecat ex aliquip reprehenderit id eiusmod
+        sint consequat consectetur aliqua quis aliquip. Sit ipsum exercitation
+        magna deserunt Lorem quis dolor. Adipisicing nostrud sint elit enim
+        consectetur qui ad sunt. Eu minim enim qui reprehenderit qui. Occaecat
+        ex aliquip reprehenderit id eiusmod sint consequat consectetur aliqua
+        quis aliquip. Sit ipsum exercitation magna deserunt Lorem quis dolor.
+        Adipisicing nostrud sint elit enim consectetur qui ad sunt. Eu minim
+        enim qui reprehenderit qui. Occaecat ex aliquip reprehenderit id eiusmod
+        sint consequat consectetur aliqua quis aliquip.
+      </p>
+    </div>
+  );
+};
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Header } from "@/components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Providers } from "./Providers";
+import { Navbar } from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,8 @@ export default function RootLayout({ children }) {
         <Providers>
           {/* Header */}
           <Header />
+          {/* Navbar */}
+          <Navbar />
           {children}
         </Providers>
       </body>
