103feef34d50b6a4db2cf52f14922d35839509b7
Nishant Singh <nishantbihar529@gmail.com>
2023-08-08 17:16:52 +0530

    feat: create header section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,22 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { Header } from "@/components/Header";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'IMDb Clone',
-  description: 'IMDb Clone developed by Nishant Singh',
-}
+  title: "IMDb Clone",
+  description: "IMDb Clone developed by Nishant Singh",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* Header */}
+        <Header />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
