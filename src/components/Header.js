701291e7c58ce8dbc281496e3efcc7da263e37ca
Nishant Singh <nishantbihar529@gmail.com>
2023-08-08 19:10:12 +0530

    feat: add dark mode

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import { MenuItem } from "./MenuItem";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
+import { DarkModeSwitch } from "./DarkModeSwitch";
 import Link from "next/link";
 
 export const Header = () => {
@@ -15,7 +16,8 @@ export const Header = () => {
           Icon={BsFillInfoCircleFill}
         />
       </div>
-      <div className="">
+      <div className="flex items-center space-x-5">
+        <DarkModeSwitch/>
         <Link href={"/"}>
           <h2 className="text-2xl">
             <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">
